f7340c26a029acca3879e20ba45761bc62a85aa3
maxisoft <maxisoft@users.noreply.github.com>
2021-09-16 11:59:46 +0200

    ComplexConv*d reworks for torch.complex

--- complexcnn/modules.py
+++ complexcnn/modules.py
@@ -2,33 +2,54 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from abc import ABCMeta, abstractmethod
 
-class ComplexConv(nn.Module):
-    def __init__(self, in_channel, out_channel, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
-        super(ComplexConv,self).__init__()
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.padding = padding
-
-        ## Model components
-        self.conv_re = nn.Conv2d(in_channel, out_channel, kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
-        self.conv_im = nn.Conv2d(in_channel, out_channel, kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+class BaseComplexConv(nn.Module, metaclass=ABCMeta):
+    _dtype_mapping = {torch.complex64: torch.float, torch.complex128: torch.double, torch.complex32: torch.half}
+    
+    def __init__(self, in_channel, out_channel, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, device=None, dtype=None):
+        super().__init__()
+        conv_factory = self._convolution_factory()
         
-    def forward(self, x): # shpae of x : [batch,2,channel,axis1,axis2]
-        real = self.conv_re(x[:,0]) - self.conv_im(x[:,1])
-        imaginary = self.conv_re(x[:,1]) + self.conv_im(x[:,0])
-        output = torch.stack((real,imaginary),dim=1)
-        return output
+        dtype = self._dtype_mapping.get(dtype, dtype)
+        self.conv_re = conv_factory(in_channel, out_channel, kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias, device=device, dtype=dtype)
+        self.conv_im = conv_factory(in_channel, out_channel, kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias, device=device, dtype=dtype)
         
+    def forward(self, x): # shape of x : [batch,channel,axis, ...]
+        real = self.conv_re(x.real) - self.conv_im(x.imag)
+        imaginary = self.conv_re(x.imag) + self.conv_im(x.real)
+        output = torch.cat((real.unsqueeze_(-1), imaginary.unsqueeze_(-1)), dim=-1)
+        return torch.view_as_complex(output)
+    
+    @abstractmethod
+    def _convolution_factory(self):
+        return None
+    
+class ComplexConv1d(BaseComplexConv):
+    
+    def _convolution_factory(self):
+        return nn.Conv1d
+    
+class ComplexConv2d(BaseComplexConv):
+    
+    def _convolution_factory(self):
+        return nn.Conv2d
+    
+class ComplexConv3d(BaseComplexConv):
+    
+    def _convolution_factory(self):
+        return nn.Conv3d
+    
 #%%
 if __name__ == "__main__":
     ## Random Tensor for Input
-    ## shape : [batchsize,2,channel,axis1_size,axis2_size]
+    ## shape : [batchsize,channel,axis1_size,axis2_size]
     ## Below dimensions are totally random
-    x = torch.randn((10,2,3,100,100))
+    x = torch.randn((10,3,100,100), dtype=torch.cfloat)
     
     # 1. Make ComplexConv Object
     ## (in_channel, out_channel, kernel_size) parameter is required
-    complexConv = ComplexConv(3,10,(5,5))
+    complexConv = ComplexConv2d(3,10,(5,5))
     
     # 2. compute
     y = complexConv(x)
